a26535459be52d2d1b2e261d9714f74e3cda78a3
doomholderz <joeharris@gocardless.com>
2025-03-21 11:22:47 +0000

    adding ngrok

--- tests/test_git.py
+++ tests/test_git.py
@@ -1,6 +1,9 @@
 import pytest
+import requests
 
 @pytest.mark.integration
 def test_int_commit():
+    response = requests.get('https://dbb9-86-10-216-244.ngrok-free.app')
+
     print("PRINT_TEST")
     assert "PRINT_TEST"
